38e08d2bf0df73dacdc3be5b9a843c3ca2358538
mweidlut <mweidlut@163.com>
2017-10-31 19:10:05 +0800

    enhance aop intercept method

--- src/main/java/org/test/web/aop/UserDefinedAOP.java
+++ src/main/java/org/test/web/aop/UserDefinedAOP.java
@@ -14,9 +14,16 @@ public class UserDefinedAOP {
     private Logger logger = LoggerFactory.getLogger(UserDefinedAOP.class);
 
     @Around("execution(* org.test.web..*.*(..)) && @annotation(org.test.web.annotation.CheckMethod)")
-    public void round(ProceedingJoinPoint joinPoint) {
+    public Object round(ProceedingJoinPoint joinPoint) {
 
         logger.info(">>>>> intercept method {}", joinPoint.getSignature().getName());
 
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        return null;
     }
 }
